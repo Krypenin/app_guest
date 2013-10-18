0ddc1445221d4d022ce31a3685e0b007d1dd5d39
Konstantin <krypenin@gmail.com>
2013-10-18 23:17:00 +0400

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 AppGuest::Application.routes.draw do
+  get "main/index"
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +42,7 @@ AppGuest::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
